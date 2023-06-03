2c2390f84bf661ae80460bc706806d1c52b2b942
Mohammad Khoulani <mhmmdkholani@gmail.com>
2023-06-03 12:49:44 +0300

    Add routing, install bootstrap and add to  project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { SingleCategoryComponent } from './components/pages/single-category/single-category.component';
+import { SinglePostComponent } from './components/pages/single-post/single-post.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { TermsAndConditionsComponent } from './components/pages/terms-and-conditions/terms-and-conditions.component';
+import { ContactusComponent } from './components/pages/contactus/contactus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'category', component: SingleCategoryComponent },
+  { path: 'post', component: SinglePostComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'terms-conditions', component: TermsAndConditionsComponent },
+  { path: 'contact', component: ContactusComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
