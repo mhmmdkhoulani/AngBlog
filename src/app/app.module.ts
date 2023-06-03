7b65f2d8f83c71eacf7b657b0ac324ab308d41af
Mohammad Khoulani <mhmmdkholani@gmail.com>
2023-06-03 12:36:00 +0300

    Create the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layouts/header/header.component';
+import { CategoryNavbarComponent } from './components/layouts/category-navbar/category-navbar.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { SingleCategoryComponent } from './components/pages/single-category/single-category.component';
+import { SinglePostComponent } from './components/pages/single-post/single-post.component';
+import { TermsAndConditionsComponent } from './components/pages/terms-and-conditions/terms-and-conditions.component';
+import { ContactusComponent } from './components/pages/contactus/contactus.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { SubscriptionFormComponent } from './components/forms/subscription-form/subscription-form.component';
+import { CommentFormComponent } from './components/comments/comment-form/comment-form.component';
+import { CommentsListComponent } from './components/comments/comments-list/comments-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CategoryNavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    SingleCategoryComponent,
+    SinglePostComponent,
+    TermsAndConditionsComponent,
+    ContactusComponent,
+    AboutComponent,
+    SubscriptionFormComponent,
+    CommentFormComponent,
+    CommentsListComponent
   ],
   imports: [
     BrowserModule,
